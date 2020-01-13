0db6ef6ba604b256dac66b9f731f184ce2c0b51f
David Schneider <david.jay.schneider@gmail.com>
2020-01-13 10:04:09 -0500

    adds schema and seed

---
+++ seed.sql
@@ -0,0 +1,20 @@
+insert into owners
+        (name, phone_number)
+    values 
+        ('chris', '1233453413'),
+        ('david', '4134564235');
+
+insert into pets
+        (name, species, birthdate)
+    values
+        ('oakley', 'cat', '2010-05-30', 1),
+        ('milla', 'tortoise', '2005-01-01', 1),
+        ('dexter', 'dog', '2003-09-01', 2),
+        ('hank', 'dog', '1999-03-14', 2),
+        ('seymour', 'cat', '1901-12-25', 2);
+
+
+
+
+
+
